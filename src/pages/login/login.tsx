bba3301bd24b9293a107ee0f701bc0e4957e403a
shivanksharma <sharmashivank82@gmail.com>
2025-09-24 23:59:15 +0530

    update the login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Checkbox, Form, Input, Layout, Space } from "antd";
+import { Button, Card, Checkbox, Flex, Form, Input, Layout, Space } from "antd";
 import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
 import AppLogo from "../../assets/Logo/AppLogo.svg";
 
@@ -34,20 +34,44 @@ function LoginPage() {
             variant="borderless"
             style={{ width: "300px" }}
           >
-            <Form>
-              <Form.Item name="username">
+            <Form initialValues={{ remember: true }}>
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Username",
+                  },
+                  {
+                    type: "email",
+                    message: "Email is not valid",
+                  },
+                ]}
+              >
                 <Input placeholder="Username" prefix={<UserOutlined />} />
               </Form.Item>
-              <Form.Item name="password">
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password",
+                  },
+                ]}
+              >
                 <Input.Password
                   placeholder="Password"
                   prefix={<LockOutlined />}
                 />
               </Form.Item>
-              <Form.Item name="remember">
-                <Checkbox>Remember me</Checkbox>
-                <a href="#">Forgot Password</a>
-              </Form.Item>
+              <Flex justify="space-between">
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="#" id="login-form-forgot">
+                  Forgot Password
+                </a>
+              </Flex>
               <Form.Item>
                 <Button
                   type="primary"
