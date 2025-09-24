af737bbdc7c71a1d6f103c0c89b0fdf2d742ca7d
shivanksharma <sharmashivank82@gmail.com>
2025-09-24 21:53:50 +0530

    create signin page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "antd/dist/reset.css";
 import "./index.css";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#F65f42",
+          colorLink: "#F65F42",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </StrictMode>
 );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,13 +1,66 @@
+import { Button, Card, Checkbox, Form, Input, Layout, Space } from "antd";
+import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
+import AppLogo from "../../assets/Logo/AppLogo.svg";
+
 function LoginPage() {
   return (
     <>
-      <h1>Sign In</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
-      <label htmlFor="remember-me">Remember me</label>
-      <input type="checkbox" id="remember-me" />
-      <a href="#">Forgot Password</a>
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Space direction="vertical" align="center" size={"large"}>
+          <Layout.Content
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={AppLogo} alt="" style={{ height: 23 }} />
+          </Layout.Content>
+          <Card
+            title={
+              <Space
+                style={{
+                  width: "100%",
+                  fontSize: 16,
+                  justifyContent: "center",
+                }}
+              >
+                <LockFilled />
+                Sign In
+              </Space>
+            }
+            variant="borderless"
+            style={{ width: "300px" }}
+          >
+            <Form>
+              <Form.Item name="username">
+                <Input placeholder="Username" prefix={<UserOutlined />} />
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password
+                  placeholder="Password"
+                  prefix={<LockOutlined />}
+                />
+              </Form.Item>
+              <Form.Item name="remember">
+                <Checkbox>Remember me</Checkbox>
+                <a href="#">Forgot Password</a>
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
     </>
   );
 }
