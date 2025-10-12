d3143df1ee3ea04796ad7328815da46344b340bb
shivanksharma <sharmashivank82@gmail.com>
2025-10-12 16:33:34 +0530

    add table-ant-design in user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,9 +1,34 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Space, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../http/api";
 import type { User } from "../types";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 
 function UserPage() {
   const {
@@ -22,22 +47,16 @@ function UserPage() {
 
   return (
     <>
-      <Breadcrumb
-        items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
-        separator={<RightOutlined />}
-      />
-      {isLoading && <h1>Loading...</h1>}
-      {isError && <div>{error.message}</div>}
-      {users && (
-        <div>
-          <h1>Users</h1>
-          <ul>
-            {users?.map((user: User) => {
-              return <li key={user.id}>{user.firstName}</li>;
-            })}
-          </ul>
-        </div>
-      )}
+      <Space direction="vertical" style={{ width: "100%" }} size="large">
+        <Breadcrumb
+          items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
+          separator={<RightOutlined />}
+        />
+        {isLoading && <h1>Loading...</h1>}
+        {isError && <div>{error.message}</div>}
+
+        <Table columns={columns} dataSource={users} />
+      </Space>
     </>
   );
 }
